03b1b6a868da2997ed055d8d99079324dbecf08d
AlanMarkBerg <alan.berg.phd@gmail.com>
2016-01-18 10:06:38 +0100

    Create area to save example code

---
+++ Code-Snippets/README.md
@@ -0,0 +1,3 @@
+# DESCRIPTION
+
+Code snippets that might be useful for me. I make no pretence of authorship, many will be discovered off the Internet. If so I will provide the link to the source. License may depend on the origin.
---
+++ Code-Snippets/swirl.R
@@ -0,0 +1,20 @@
+# See: http://swirlstats.com
+
+# One time only
+#install.packages("swirl")
+
+library(swirl)
+
+# One time only: Install the courses you like
+#install_from_swirl("Mathematical Biostatistics Boot Camp")
+#install_from_swirl("Getting and Cleaning Data")
+#install_from_swirl("Regression Models")
+#install_from_swirl("Data Analysis")
+#install_from_swirl("Mathematical Biostatistics Boot Camp")
+#install_from_swirl("Statistical Inference")
+#install_from_swirl("R Programming")
+#install_from_swirl("Open Intro")
+
+# Start the commandline
+swirl()
+
